fd15554908694587fea08a281d8a1eb7bcce5a1c
Ehtisham <ehtishamiqbal433@gmail.com>
2026-08-06 09:31:55 +0000

    test: add initial unit test for main module

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,5 +5,5 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent / "cli"))
 import main
 
-def test_main_module():
+def test_main_module_load():
     assert main is not None
