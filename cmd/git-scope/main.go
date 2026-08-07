727b44d11ce2ae30807fc190bf63f39195431200
Ehtisham <ehtishamiqbal433@gmail.com>
2026-08-07 08:22:33 +0000

    chore: set module path to github.com/ehtishamiqbal433-cell/Git-Scope, update imports, remove committed binary, ignore binaries

--- cmd/git-scope/main.go
+++ cmd/git-scope/main.go
@@ -1,1 +1,34 @@
-[...paste the full cmd/git-scope/main.go exact content...]
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ehtishamiqbal433-cell/Git-Scope/internal/tui"
+)
+
+var version = "v1.0.0"
+
+func main() {
+	ver := flag.Bool("version", false, "print version")
+	flag.Parse()
+
+	if *ver {
+		fmt.Println(version)
+		return
+	}
+
+	// If any args passed (non-flag), keep behavior for compatibility
+	if len(os.Args) > 1 {
+		for _, a := range os.Args[1:] {
+			if a == "--version" {
+				fmt.Println(version)
+				return
+			}
+		}
+	}
+
+	fmt.Println("Starting Git-Scope interactive UI (stub). Press Ctrl+C to exit.")
+	tui.Run()
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/ehtishamiqbal433-cell/infra-scope
+module github.com/ehtishamiqbal433-cell/Git-Scope
 
 go 1.22
